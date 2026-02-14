d8968182d2c0856dd1e1ca382c5fc39b917e8620
ayushyelne <yelne.ayush@gmail.com>
2026-02-14 12:47:03 -0800

    Setting up pipeline

--- lib/ci-cd-aws-pipeline-demo-stack.ts
+++ lib/ci-cd-aws-pipeline-demo-stack.ts
@@ -1,16 +1,23 @@
 import * as cdk from 'aws-cdk-lib/core';
+import { CodePipeline, CodePipelineSource, ShellStep} from 'aws-cdk-lib/pipelines';
+import { ManualApprovalStep} from "aws-cdk-lib/pipelines";
+// import { MyPipelineAddStage} from './stages';
 import { Construct } from 'constructs';
+import {Code} from "aws-cdk-lib/aws-lambda";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CiCdAwsPipelineDemoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CiCdAwsPipelineDemoQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new CodePipeline(this, 'Pipeline', {
+      pipelineName: 'TestPipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('ayushyelne/ci-cd-aws-pipeline-demo', 'main'),
+        commands: ['npm ci',
+        'npm run build',
+        'npm cdk synth']
+      }),
+    });
   }
 }
